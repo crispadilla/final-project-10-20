b3107ddde9ad2438f00f8bb56985bb0af7d594cd
Cris Padilla <crispadilla82@gmail.com>
2020-11-17 14:59:13 -0800

    Added right tile shift functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var tileArray;
 const numTiles = document.body.getElementsByClassName("tile").length;
+const tileShiftAmt = 105; // Unit is pixels
 
 setupGame();
 
@@ -20,10 +21,10 @@ function setupGame() {
       index += 1;
     }
   }
-  setAttributes();
+  setTileAttributes();
 }
 
-function setAttributes() {
+function setTileAttributes() {
   tileArray.forEach((element) =>
     element.setAttribute("onclick", "moveTile(this)")
   );
@@ -35,4 +36,43 @@ function setupEmptyTile(tile) {
 
 function moveTile(tile) {
   console.log(tile.innerText);
+  if (moveRightAllowed(tile)) moveRight(tile);
+}
+
+function moveRightAllowed(tile) {
+  var tileIndex = tileArray.indexOf(tile);
+  var indexOfTileToTheRight = tileIndex + 1;
+  if (
+    tileIndex != 2 &&
+    tileIndex != 5 &&
+    tileIndex != 8 &&
+    tileArray[indexOfTileToTheRight].classList.contains("empty")
+  ) {
+    console.log("Ok to move to the Right");
+    return true;
+  }
+  return false;
+}
+
+function moveRight(tile) {
+  var tileIndex = tileArray.indexOf(tile);
+  var emptyTileIndex = tileIndex + 1;
+  var emptyTile = tileArray[emptyTileIndex];
+
+  //  Move tile to the right.
+  tile.style.transition = "transform 1.0s ease-in-out";
+  tile.style.transform = "translateX(" + `${tileShiftAmt}` + "px)";
+
+  //  Move empty tile to the left.
+  emptyTile.style.transition = "transform 1.0s ease-in-out";
+  emptyTile.style.transform = "translateX(-" + `${tileShiftAmt}` + "px)";
+
+  var temp = emptyTile;
+  tileArray[emptyTileIndex] = tile;
+  tileArray[tileIndex] = temp;
+
+  console.log("Former empty position new element");
+  console.log(tileArray[emptyTileIndex]);
+  console.log("Former tile positoin new element");
+  console.log(tileArray[tileIndex]);
 }
