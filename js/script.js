385878601ddde5cf3b3b18beffe27f351e140e3f
Cris Padilla <crispadilla82@gmail.com>
2020-11-17 12:55:03 -0800

    Added random number generator and randomly assigned numbers to the div tiles on the board.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 var tileArray;
+const numTiles = 9;
 
 setupGame();
 
 function setupGame() {
   tileArray = Array.from(document.body.getElementsByClassName("tile"));
-}
+  var arrayValues = Array(numTiles);
+  arrayValues.fill(true, 0);
 
-console.log(tileArray);
+  var index = 0;
+  var randNum;
+  while (index < numTiles) {
+    randNum = Math.floor(Math.random() * numTiles);
+    if (arrayValues[randNum]) {
+      tileArray[index].innerText = randNum;
+      arrayValues[randNum] = false;
+      index += 1;
+    }
+  }
+}
