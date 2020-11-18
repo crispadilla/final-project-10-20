8a25731cb59e9285fb0f1e6bd375cc98ba292229
Cris Padilla <crispadilla82@gmail.com>
2020-11-17 22:10:21 -0800

    Added a black background

--- js/script.js
+++ js/script.js
@@ -10,25 +10,11 @@ setupGame();
 
 function setupGame() {
   tileArray = Array.from(document.body.getElementsByClassName("tile"));
-  var arrayValues = Array(numTiles);
-  arrayValues.fill(true, 0);
 
   setupCoordinates();
   positionTiles();
   setTileAttributes();
-
-  var index = 0;
-  var randNum;
-  while (index < numTiles) {
-    randNum = Math.floor(Math.random() * numTiles);
-    if (arrayValues[randNum]) {
-      randNum == 0
-        ? setupEmptyTile(tileArray[index])
-        : (tileArray[index].innerText = randNum);
-      arrayValues[randNum] = false;
-      index += 1;
-    }
-  }
+  assignTileNumbers();
 }
 
 function setupCoordinates() {
@@ -43,6 +29,24 @@ function setupCoordinates() {
     }
     elementCoordinatesArray[i] = [x_coordinate, y_coordinate];
     x_coordinate += tileHorizontalShiftAmt;
+  }
+}
+
+function assignTileNumbers() {
+  var arrayValues = Array(numTiles);
+  arrayValues.fill(true, 0);
+
+  var index = 0;
+  var randNum;
+  while (index < numTiles) {
+    randNum = Math.floor(Math.random() * numTiles);
+    if (arrayValues[randNum]) {
+      randNum == 0
+        ? setupEmptyTile(tileArray[index])
+        : (tileArray[index].innerText = randNum);
+      arrayValues[randNum] = false;
+      index += 1;
+    }
   }
 }
 
